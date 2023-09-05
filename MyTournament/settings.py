30759f898bd4c534fb1cbb90670080f3308c71aa
master <danielgomes312y@gmail.com>
2023-09-05 20:17:10 -0300

    Creating a new project without toolbar
    because was causing too many pronlemas.

--- MyTournament/settings.py
+++ MyTournament/settings.py
@@ -37,12 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground',
-    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- playground/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
