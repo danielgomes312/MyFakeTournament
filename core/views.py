90d58b3e81e64b6b2d746f46af1062b905bb9caf
master <danielgomes312y@gmail.com>
2023-09-06 20:26:43 -0300

    Making another test

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# for some reasons my github its not recognizing my commits and pushs
 
 def index(request):
     return render(request, 'index.html')
